31463f8546f35db58d2fff04597e56020a24e9dc
Amir <aleahmad.amir@gmail.com>
2018-03-27 11:26:50 +0430

    Initial commit (install gulp, bootstrap, FAwsome, Modules)

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const gulp         = require('gulp');
+const sass         = require('gulp-sass');
+const cleanCss     = require('gulp-clean-css');
+const rename       = require('gulp-rename');
+const uglify       = require('uglify-js');
+const sourcemaps   = require('gulp-sourcemaps');
+
